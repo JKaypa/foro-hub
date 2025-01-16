69d8a93266f0b032722cbdeec631619fa2dede49
JKaypa <jose_kaypa@hotmail.com>
2025-01-16 14:17:49 -0500

    change version number

--- src/main/resources/db/migration/V3__create-table-user.sql
+++
@@ -1,8 +0,0 @@
-create table user(
-  id bigint not null auto_increment,
-  name varchar(255) not null;
-  email varchar(255) not null unique,
-  password varchar(255) not null,
-
-  primary key (id)
-);
