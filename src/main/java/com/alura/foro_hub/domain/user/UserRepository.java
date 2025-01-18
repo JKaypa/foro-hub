55707edd8f1cffc72091cbda8c0064d1040522c6
JKaypa <jose_kaypa@hotmail.com>
2025-01-17 22:37:37 -0500

    find by email

--- src/main/java/com/alura/foro_hub/domain/user/UserRepository.java
+++ src/main/java/com/alura/foro_hub/domain/user/UserRepository.java
@@ -1,7 +1,10 @@
 package com.alura.foro_hub.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  UserDetails findByEmail(String email);
 
 }
