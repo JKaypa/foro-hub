23b223774ce528ad0fa5733179ca7ae7391f7f5b
JKaypa <jose_kaypa@hotmail.com>
2025-01-20 10:38:24 -0500

    change optional type

--- src/main/java/com/alura/foro_hub/domain/user/UserRepository.java
+++ src/main/java/com/alura/foro_hub/domain/user/UserRepository.java
@@ -1,10 +1,14 @@
 package com.alura.foro_hub.domain.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  UserDetails findByEmail(String email);
+  Optional<User> findByEmail(String email);
 
 }
