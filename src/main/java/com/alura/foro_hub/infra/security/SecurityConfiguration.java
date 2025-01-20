4bc78cfeabfa594504ed1bb9ce32d0e1c5caf8a1
JKaypa <jose_kaypa@hotmail.com>
2025-01-20 10:34:46 -0500

    create basic security config

---
+++ src/main/java/com/alura/foro_hub/infra/security/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.alura.foro_hub.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.alura.foro_hub.domain.user.UserRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    return httpSecurity.csrf(csrf -> csrf.disable())
+    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    .build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+    return authConfig.getAuthenticationManager();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(UserRepository userRepository) {
+      return email -> userRepository.findByEmail(email)
+      .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+}
