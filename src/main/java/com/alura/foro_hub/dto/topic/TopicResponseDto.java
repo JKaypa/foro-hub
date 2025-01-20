9ca2f62b56b019ef5bb1bff6e6b7f0528c8fe703
JKaypa <jose_kaypa@hotmail.com>
2025-01-20 10:37:55 -0500

    create topic response

---
+++ src/main/java/com/alura/foro_hub/dto/topic/TopicResponseDto.java
@@ -0,0 +1,25 @@
+package com.alura.foro_hub.dto.topic;
+
+import java.time.LocalDateTime;
+
+import com.alura.foro_hub.domain.topic.Status;
+import com.alura.foro_hub.domain.topic.Topic;
+import com.alura.foro_hub.dto.user.UserResponseDto;
+
+public record TopicResponseDto(
+    Long id,
+    String title,
+    String description,
+    UserResponseDto author,
+    Status status,
+    LocalDateTime createdAt) {
+  public TopicResponseDto(Topic topic) {
+    this(
+        topic.getId(),
+        topic.getTitle(),
+        topic.getDescription(),
+        new UserResponseDto(topic.getAuthor()),
+        topic.getStatus(),
+        topic.getCreatedAt());
+  }
+}
